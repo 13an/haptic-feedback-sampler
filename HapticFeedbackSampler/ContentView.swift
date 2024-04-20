1a3d5ae45139ec7503ff578c71151fb251528293
13an <banjoyatakumi@gmail.com>
2024-04-20 14:15:48 +0900

    reordered files

--- HapticFeedbackSampler/ContentView.swift
+++ HapticFeedbackSampler/ContentView.swift
@@ -34,11 +34,11 @@ struct HapticButton: View {
             Text("\(fileName)")
                 .font(.title3)
                 .foregroundStyle(.white)
+                .padding(EdgeInsets(top: 16, leading: 24, bottom: 16, trailing: 24))
+                .frame(width: 240)
+                .background(.blue)
+                .clipShape(.rect(cornerRadius: 16, style: .continuous))
         }
-        .padding(EdgeInsets(top: 16, leading: 24, bottom: 16, trailing: 24))
-        .frame(width: 240)
-        .background(.blue)
-        .clipShape(.rect(cornerRadius: 16, style: .continuous))
         .padding(16)
     }
 }
